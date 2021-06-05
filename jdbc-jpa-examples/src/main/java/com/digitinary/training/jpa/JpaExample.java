8b42c35a12a7cc37d54890572d1afc73f921a636
s_msameh@yahoo.com <sammjuba>
2021-06-05 19:32:50 +0300

    added persistence.xml file

--- jdbc-jpa-examples/src/main/java/com/digitinary/training/jpa/JpaExample.java
+++ jdbc-jpa-examples/src/main/java/com/digitinary/training/jpa/JpaExample.java
@@ -1,11 +1,8 @@
 package com.digitinary.training.jpa;
 
 import javax.persistence.EntityManager;
-
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  * 
@@ -17,22 +14,42 @@ public class JpaExample {
 	private static EntityManager em = null;
 	
 	static {
-		// A SessionFactory is set up once for an application!
-		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-				.configure() // configures settings from hibernate.cfg.xml
-				.build();
+		//Hibernate way
+//		// A SessionFactory is set up once for an application!
+//		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+//				.configure() // configures settings from hibernate.cfg.xml
+//				.build();
+//		try {
+//			SessionFactory sessionFactory = new MetadataSources(registry)
+//				.buildMetadata()
+//				.buildSessionFactory();
+//			
+//			em = sessionFactory.createEntityManager();
+//		}
+//		catch (Exception e) {
+//			// The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+//			// so destroy it manually.
+//			e.printStackTrace();
+//			StandardServiceRegistryBuilder.destroy(registry);
+//		}
+		
+		//Standard JPA Way
+		EntityManagerFactory emf = null;
+		
 		try {
-			SessionFactory sessionFactory = new MetadataSources(registry)
-				.buildMetadata()
-				.buildSessionFactory();
+			emf = Persistence.createEntityManagerFactory("JpaExample");
+			em = emf.createEntityManager();
+		} catch (Exception e) {
 			
-			em = sessionFactory.createEntityManager();
-		}
-		catch (Exception e) {
-			// The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
-			// so destroy it manually.
 			e.printStackTrace();
-			StandardServiceRegistryBuilder.destroy(registry);
+			
+			if(em != null && em.isOpen()) {
+				em.close();
+			}
+			
+			if(emf != null && emf.isOpen()) {
+				emf.close();
+			}
 		}
 	}
 	
@@ -59,8 +76,8 @@ public class JpaExample {
 		
 		Customer c16 = em.find(Customer.class, Long.valueOf(16));
 		
-		c16.setCustomerName("Modified Customer");
-		em.merge(c16);
+		//c16.setCustomerName("Modified Customer");
+		//em.merge(c16);
 		
 		//em.persist(newCustomer);
 		//em.remove(newCustomer);
